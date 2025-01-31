41932504fe2317022ed1e791c8558f955e915a41
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-01-31 15:36:13 -0300

    correção

--- ex013.py
+++ ex013.py
@@ -1,1 +1,7 @@
-#faça um algoritimo que leia o salario de um funcionario e mostre seu novo salario com 15% de almento.
+#faça um algoritimo que leia o salario de um funcionario e mostre seu novo salario com 15% de aumento.
+
+salario = float(input('qual o seu salario?: '))
+
+aumento = salario * 0.15
+nv_salario = aumento + salario 
+print('o seu salario será de: {}'.format(nv_salario))
--- ex014.py
+++ ex014.py
@@ -1,1 +1,3 @@
- # conversor de temperatura assista a aula 
+c = float(input('informe a temperatura em °C: '))
+f = ((9*c) / 5 )+ 32
+print('A temperatura de {}°C cpresponde a {}°F!'.format(c,f))
