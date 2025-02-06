f702a74fc3dcce496f8bf8c9e809850ab04f7703
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-06 03:26:40 -0300

    Update no desafio033

--- ex033.py
+++ ex033.py
@@ -1,18 +1,18 @@
 #faça um program que leia três numeros e mostre qual é o maior e o menor deles.
-a = int(input('Primeiro valor: '))
-b = int(input('Segundo valor: '))
-c = int(input('Terceiro valor: '))
-# Verificando quem é menor
-menor = a
-if b < a and b < c:
-    menor = b
-if c < a and c < b:
-    menor = c
-# Verificando quem é o maior
-maior = a
-if b > a and b > c:
-    maior = b
-if c > a and c > b:
-    maior = c
-print('O menor valor digitado foi {}'.format(menor))
-print('O maior valor digitado foi {}'.format(maior))
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Seugundo valor: '))
+n3 = int(input('Terceiro valor: '))
+menor = n1
+if n2<n1 and n2<n3:
+    menor = n2
+if n3<n1 and n3<n2:
+    menor = n3 
+if n1>n2 and n1>3:
+    maior = n1
+if n2>n1 and n2>n3:
+    maior = n2
+if n3>n1 and n3>n2:
+    maior = n3
+print('O menor numero é {}'.format(menor))
+print('O maior numero é {}'.format(maior))
