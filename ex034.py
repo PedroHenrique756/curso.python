3d059167e2b775e1b30d8a517077c854937bd06d
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-06 03:58:13 -0300

    ajuste no ex34

--- ex034.py
+++ ex034.py
@@ -3,4 +3,4 @@ if salário <= 1250:
     novo = salário + (salário * 15 / 100)
 else:
     novo = salário + (salário * 10 / 100)
-print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salário, novo))
+print('Quem ganhava R${} passa a ganhar R${} agora.'.format(salário, novo))
