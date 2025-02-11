9d48723c1f88e5488dfc9f863f06741a444fde64
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-11 08:24:21 -0300

    Update ex041.py

--- ex041.py
+++ ex041.py
@@ -8,4 +8,17 @@ nascimento de um atleta e mostre sua categoria, de acordo com a idade:
 - Até 20 anos: SÊNIOR
 - Acima: MASTER
 """
-
+from datetime import date
+ano = int(input('Qual o ano que você nasceu?: '))
+ano_atual = date.today().year
+idade =  ano_atual - ano
+if idade < 9:
+    print('Ele é um atleta MIRIM')
+elif idade < 14:
+    print('Ele é um atleta INFANTIL')
+elif idade < 19:
+    print('Ele é um atleta JÚNIOR')
+elif idade < 20:
+    print('Ele é um atleta SÊNIOR')
+else:
+    print('Ele é MESTRE')
