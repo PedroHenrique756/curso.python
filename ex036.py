f2e4c0a6006babc2f2ec2911a64fb870adcc85c9
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-16 15:00:35 -0300

    aprovando empréstimo

--- ex036.py
+++ ex036.py
@@ -2,10 +2,10 @@
 
 valor_casa = float(input('Qual o valor da casa?: R$'))
 salario = float(input('Qual o seu salario?: R$'))
-parcelas = int(input('Qualtas vezes vc quer parcelar?: '))
+parcela = int(input('Qualtas vezes vc quer parcelar?: '))
 prestação = valor_casa / (parcelas *12)
 minimo = salario *30 / 100
-print('Para pagar uma casa de R${:.2f} e {} anos, á prestação será de R${:.2f}'.format(valor_casa, parcelas, prestação))
+print('Para pagar uma casa de R${:.2f} e {} anos, á prestação será de R${:.2f}'.format(valor_casa, parcela, prestação))
 if prestação <=minimo:
     print('Emprestimo pode ser consedido')
 else:
