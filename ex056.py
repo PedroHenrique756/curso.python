c0777ee3a7464f36663180f512bb2d06f7c14610
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-16 18:52:07 -0300

    Create ex056.py

---
+++ ex056.py
@@ -0,0 +1,6 @@
+'''
+faça um programa que leia nome, idade e sexo de 4 pessoas. No final o programa mostra:
+-A media de idade do grupo 
+-qual o nome do home mais velho 
+-quantas mulhes tem menos de 20 anos
+'''
