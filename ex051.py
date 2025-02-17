ce26775ea616061f61bf802c82912e7a7643551b
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-17 14:04:25 -0300

    PA

--- ex051.py
+++ ex051.py
@@ -1,1 +1,7 @@
 #desenvolva um programa que leia o primeiro termo e a razão de uma PA. no final, mostre os 3 primeiros termos dessa progressão 
+primeiro = int(input('Primeiro termo: ')) 
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao # enésimo termo de uma PA
+for c in range(primeiro, decimo + razao, razao):
+    print('{}'.format(c), end='-> ')
+print('ACABOU')
