2b324e5bde69d34d6e61dc45b52ca8933ce190cf
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-17 18:46:05 -0300

    Maior e menor da sequência

--- ex055.py
+++ ex055.py
@@ -1,1 +1,14 @@
 #faça um programa que leia o peso de 5 pessoas e mostre qual foi o maior e o menor entre eles 
+maior = 0
+menor = 0
+for p in range(1, 6):
+    peso = float(input('peso da {}ª pessoa?: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso foi de {}Kg e o menor peso foi de {}Kg'.format(maior, menor))
