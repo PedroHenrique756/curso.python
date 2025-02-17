7ecd4b255ad0eb1726193f718768e801a3a20d12
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-17 13:44:57 -0300

    soma dos pares

--- ex050.py
+++ ex050.py
@@ -1,1 +1,7 @@
 #desenvolva um program que leia 6 numeros e retorne a soma dos numeros, mas somente os pares desconsiderando os imparis
+soma = 0
+for c in range(1, 7):
+    num = int(input('Qual o {} valor'.format(c)))
+    if num % 2 == 0:
+        soma += num
+print('A soma de 6 numeros e a soma foi {}'.format(soma))
