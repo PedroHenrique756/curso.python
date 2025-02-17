1597607dab99384a85c203923f6caa42bc4f67af
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-17 18:45:10 -0300

    Detector de palíndromo

--- ex053.py
+++ ex053.py
@@ -1,1 +1,12 @@
 #faça um programa que leia uma frase qualquer e retorne se ela é ou nao um palindromo, desconsiderando os espaços.
+frase = str(input('Qual a frase?: ')).strip() .upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print(junto, inverso)
+if inverso == junto:
+    print('Temos um palindromo!')
+else:
+    print('Sua plavra não é um palindromo ')
