d7d23f40017cb0c565d8d4020c21f9b89c5de2be
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-18 19:18:44 -0300

    validação de dados

--- ex057.py
+++ ex057.py
@@ -1,1 +1,6 @@
-#Faça um programa que leia o sexo de uma pessoa, mas so aceite F ou M, caso esteja errado peça a digitação novamente até um valor valido.
+#Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+sexo = str(input('Informe seu sexo [M/F]: ')).strip() .upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Invalido!, informe seu sexo novamente [M/F]: ')).strip() .upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo))
