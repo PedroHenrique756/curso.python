22077acd063fad146feb5743cdbc8d0929f6dc29
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-18 18:26:07 -0300

    criando um menu de opções

---
+++ ex059.py
@@ -0,0 +1,10 @@
+'''
+Crie um program que leia dois valores e mostre um menu na tela
+[ 1 ]somar 
+[ 2 ]multiplicar
+[ 3 ]maior 
+[ 4 ]novos numeros
+[ 5 ]sair do programa
+
+seu programa deverá relizar a operação escolhida
+'''
