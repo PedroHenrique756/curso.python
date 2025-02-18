8bcf89bf3fbf91f1c69eb6849e2502054737518a
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-18 18:25:41 -0300

    jogo da adivinhação V2.0

---
+++ ex058.py
@@ -0,0 +1,1 @@
+#Melhore o jogo do desafio 28 onde o computador vai pesar em um numero entre 0 a 10, so que agora o jogador vai jogar ate adivinhar, mostrando no final quantos palpites foram necessário para vencer.
