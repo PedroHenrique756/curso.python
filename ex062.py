465ec6dd158389a58ca9af82ae3a5906bc930958
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-19 18:39:11 -0300

    Super Progressão Aritmética v3.0

--- ex062.py
+++ ex062.py
@@ -1,1 +1,18 @@
 #melhore o desafio 61, pergunte ao usuario se ele quer mostrar mais algum termo, o programa encerra quando ele disse que quer mostrar 0 termo.
+
+primeiro = int(input('Primeiro termo: ')) 
+razao = int(input('Razão: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total+=mais
+    while cont <= 10:
+        print('{} ->'.format(termo), end='')
+        termo += razao
+        cont +=1
+    print('PAUSA.')
+    mais = int(input('Quantos termos você quer mostrar a mais?: '))
+print('FIM.')
