5f32c22bdfcddb2a008bbb3617ca281a8d05d66a
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-20 13:56:24 -0300

    Sequência de Fibonacci v1.0

--- ex063.py
+++ ex063.py
@@ -1,2 +1,13 @@
 #escreva um programa que leia um numero n inteiro qualquer e mostre na tela os n primeiros elementos de uma sequencia de fibonacci.
-#   
+
+
+n = int(input("Quantos termos da sequência de Fibonacci você quer ver? "))
+
+a, b = 0, 1
+print("Sequência de Fibonacci:", end=" ")
+
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
+
+print()
