5fffdb79a1f1e1895bd99a03c475c4f3b7b4f6b0
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-24 06:31:29 -0300

    Tabuada v3.0

--- ex067.py
+++ ex067.py
@@ -1,1 +1,18 @@
 #Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+print('-=-' *10)
+print('TABUADA')
+print('-=-' *10)
+while True:
+    numero = int(input('Digite um numero (negativo para parar): '))
+
+    if numero < 0:
+        print('programa finalizado')
+        break
+
+    print(f'A tabuada de {numero} é')
+    for i in range(1, 11):
+        print(f'{numero} x {i} = {numero * i}')
+    print('-=-' *20)
+    print('NOVAMENTE')
+    print('-=-' *20)
