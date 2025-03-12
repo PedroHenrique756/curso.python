8ca8bbd98a0bbaca6eae02cefe29270271f9b7c2
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-03-12 12:29:26 -0300

    Análise de dados em uma Tupla

--- ex075.py
+++ ex075.py
@@ -1,7 +1,24 @@
-'''Exercício Python 075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+'''Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
 
 A) Quantas vezes apareceu o valor 9.
 
 B) Em que posição foi digitado o primeiro valor 3.
 
 C) Quais foram os números pares.'''
+
+numeros = (
+    int(input("Digite o primeiro número: ")),
+    int(input("Digite o segundo número: ")),
+    int(input("Digite o terceiro número: ")),
+    int(input("Digite o quarto número: "))
+)
+
+print("\nOs números digitados foram:", numeros)
+print("O número 9 apareceu", numeros.count(9), "vezes.")
+
+if 3 in numeros:
+    print("O primeiro número 3 foi digitado na posição", numeros.index(3) + 1)
+else:
+    print("O número 3 não foi digitado.")
+
+print("Números pares:", [num for num in numeros if num % 2 == 0])
